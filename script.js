b8ae154442e6a4c29a06eca575875cb9e275ef36
Pablotuo <43593145+Pablotuo@users.noreply.github.com>
2025-03-04 18:18:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Lista de lugares con latitudes, longitudes, nombres, descripciones y la URL de las imágenes
 var lugares = [
-    { lat: 40.4168, lon: -3.7038, nombre: "Madrid", descripcion: "La capital de España.", imagen: "images/20220501_122303.jpg" },
-    { lat: 41.9028, lon: 12.4964, nombre: "Roma", descripcion: "La ciudad eterna en Italia.", imagen: "images/20220501_122303jpg" },
-    { lat: 48.8566, lon: 2.3522, nombre: "París", descripcion: "La ciudad de la luz en Francia.", imagen: "images/20220501_122303.jpg" },
-    { lat: 34.0522, lon: -118.2437, nombre: "Los Ángeles", descripcion: "La ciudad de las estrellas en USA.", imagen: "images/20220501_122303.jpg" }
+    { lat: 40.4168, lon: -3.7038, nombre: "Madrid", descripcion: "La capital de España.", imagen: "https://github.com/Pablotuo/mis-lugares/blob/main/images/20220501_122303.jpg" },
+    { lat: 41.9028, lon: 12.4964, nombre: "Roma", descripcion: "La ciudad eterna en Italia.", imagen: "https://github.com/Pablotuo/mis-lugares/blob/main/images/20220501_122303.jpg" },
+    { lat: 48.8566, lon: 2.3522, nombre: "París", descripcion: "La ciudad de la luz en Francia.", imagen: "https://github.com/Pablotuo/mis-lugares/blob/main/images/20220501_122303.jpg" },
+    { lat: 34.0522, lon: -118.2437, nombre: "Los Ángeles", descripcion: "La ciudad de las estrellas en USA.", imagen: "https://github.com/Pablotuo/mis-lugares/blob/main/images/20220501_122303.jpg }
 ];
 
 // Iterar sobre el arreglo y agregar un marcador para cada lugar
