820766f9b83f7c783591f7bf0ebd1e8af5703d8e
Pablotuo <43593145+Pablotuo@users.noreply.github.com>
2025-03-04 18:10:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Lista de lugares con latitudes, longitudes, nombres y descripciones
+// Lista de lugares con latitudes, longitudes, nombres, descripciones y la URL de las imágenes
 var lugares = [
-    { lat: 40.4168, lon: -3.7038, nombre: "Madrid", descripcion: "La capital de España." },
-    { lat: 41.9028, lon: 12.4964, nombre: "Roma", descripcion: "La ciudad eterna en Italia." },
-    { lat: 48.8566, lon: 2.3522, nombre: "París", descripcion: "La ciudad de la luz en Francia." },
-    { lat: 34.0522, lon: -118.2437, nombre: "Los Ángeles", descripcion: "La ciudad de las estrellas en USA." }
+    { lat: 40.4168, lon: -3.7038, nombre: "Madrid", descripcion: "La capital de España.", imagen: "images/madrid.jpg" },
+    { lat: 41.9028, lon: 12.4964, nombre: "Roma", descripcion: "La ciudad eterna en Italia.", imagen: "images/roma.jpg" },
+    { lat: 48.8566, lon: 2.3522, nombre: "París", descripcion: "La ciudad de la luz en Francia.", imagen: "images/paris.jpg" },
+    { lat: 34.0522, lon: -118.2437, nombre: "Los Ángeles", descripcion: "La ciudad de las estrellas en USA.", imagen: "images/los-angeles.jpg" }
 ];
 
 // Iterar sobre el arreglo y agregar un marcador para cada lugar
 lugares.forEach(function(lugar) {
     L.marker([lugar.lat, lugar.lon]).addTo(map)
-        .bindPopup("<b>" + lugar.nombre + "</b><br>" + lugar.descripcion);
+        .bindPopup("<b>" + lugar.nombre + "</b><br>" + lugar.descripcion + "<br><img src='" + lugar.imagen + "' alt='" + lugar.nombre + "' style='width:100px; height:auto;'>");
 });
