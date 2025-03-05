c35ff7cec24f41a644f9d1e85f897fe9f10cd3ea
Pablotuo <43593145+Pablotuo@users.noreply.github.com>
2025-03-04 18:28:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Lista de lugares con latitudes, longitudes, nombres, descripciones y la URL de las imágenes
 var lugares = [
-    { lat: 40.4168, lon: -3.7038, nombre: "Madrid", descripcion: "La capital de España.", imagen: "https://github.com/Pablotuo/mis-lugares/raw/main/images/20220501_122303.jpg" },
-    { lat: 41.9028, lon: 12.4964, nombre: "Roma", descripcion: "La ciudad eterna en Italia.", imagen: "https://github.com/Pablotuo/mis-lugares/raw/main/images/20220501_122303.jpg" },
-   
+    { lat: 10.0000, lon: -84.0000, nombre: "Madrid", descripcion: "La capital de España.", imagen: "images/madrid.jpg" },
+    { lat: 9.0000, lon: -83.0000, nombre: "Roma", descripcion: "La ciudad eterna en Italia.", imagen: "images/roma.jpg" },
+    { lat: 10.0000, lon: -83.0000, nombre: "París", descripcion: "La ciudad de la luz en Francia.", imagen: "images/paris.jpg" },
+    { lat: 9.0000, lon: -84.0000, nombre: "Los Ángeles", descripcion: "La ciudad de las estrellas en USA.", imagen: "images/los-angeles.jpg" }
 ];
 
 // Iterar sobre el arreglo y agregar un marcador para cada lugar
