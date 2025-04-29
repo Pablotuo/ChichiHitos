2b4bc04383185b0a253169b8f6b8c5ee808859b2
Pablotuo <43593145+Pablotuo@users.noreply.github.com>
2025-04-29 17:48:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,26 +9,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Lista de lugares con latitudes, longitudes, nombres, descripciones y la URL de las imágenes
 var lugares = [ 
-  { 
-    "lat": 10.056087,
-    "lon": -84.008153,
-    "nombre": "309-A",
-    "descripcion": "Amarillo",
-    "imagen": "https://via.placeholder.com/100x60?text=309-A"
-  },
-  {
-    "lat": 9.99312,
-    "lon": -83.025022,
-    "nombre": "J-81",
-    "descripcion": "Amarillo",
-    "imagen": "https://via.placeholder.com/100x60?text=J-81"
-  },
+
   {
     "lat": 9.992554,
     "lon": -83.024251,
     "nombre": "57-LA",
     "descripcion": "Blanco",
-    "imagen": "https://via.placeholder.com/100x60?text=57-LA"
+    "imagen": "https://github.com/Pablotuo/ChichiHitos/blob/main/imagenes/image%20(3).png?raw=true"
   }
 ];
 
